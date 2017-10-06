0e300dd7fa537c42f12719643206e2ef0c186004
Keith Smith <aeiouandsometimesy@me.com>
2017-10-06 16:31:44 -0400

    Imported and updated!

--- PRG-3-12-Celsius-to-Fahrenheit/main.cpp
+++ PRG-3-12-Celsius-to-Fahrenheit/main.cpp
@@ -5,11 +5,40 @@
 //  Created by Keith Smith on 10/6/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Write a program that converts Celsius temperatures to Fahrenheit temperatures. The
+//  formula is
+//  F = (9/5)C + 32
+//  F is the Fahrenheit temperature, and C is the Celsius temperature
 
 #include <iostream>
+#include <iomanip>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
+    // Declare variables
+    float fltTempCelsius,
+          fltTempFahrenheit;
+    
+    
+    cout << "Please enter a temperature in degrees Celsius: " << endl;
+    cin >> fltTempCelsius;
+    while(!cin)
+    {
+        cout << "Please enter a number value:\n";
+        cin.clear();
+        cin.ignore();
+        cin >> fltTempCelsius;
+    }
+    
+    fltTempFahrenheit = ((9.0f/5.0f) * fltTempCelsius) + 32.0f;
+    
+    cout <<  setprecision(1) << fixed << showpoint;
+    
+    cout << fltTempCelsius << "C is approximately " << fltTempFahrenheit << "F\n";
+    
     return 0;
 }
+
+
